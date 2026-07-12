16b07d8e11460b8a821aca47166990c8f68a17f0
jjblanquer-netizen <jjblanquer@gmail.com>
2026-07-12 11:27:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SplitGuasa Service Worker
-const CACHE = 'splitguasa-v6';
+const CACHE = 'splitguasa-v7';
 const ASSETS = [
   '/Splitguasa/index-firebase.html',
   '/Splitguasa/manifest-firebase.json',
